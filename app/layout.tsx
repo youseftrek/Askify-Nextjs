7affd06dadbe2806ecbd76be07623050255db8cb
youseftrek <youseftarek.business@gmail.com>
2023-12-04 21:03:13 +0200

    build: setup layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,28 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Askify",
+  description:
+    "Askify is a Stack Overflow clone made by yousef tarek a full stack developer using NextJs",
+  icons: "/assets/images/site-logo.svg",
+};
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +32,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is a test font</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
